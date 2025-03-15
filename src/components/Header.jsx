e4387bee26e9dbfa7b63e55a0358023f84e4bfbc
Petros Kouzoupis <peterkuzu@gmail.com>
2025-03-15 13:22:41 +0200

    Starting project

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Find your film</h1>
+      <nav>
+        <span>My Watchlist</span>
+      </nav>
+    </header>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <section>
+        <form action="">
+          <input type="text" />
+          <button>Search</button>
+        </form>
+      </section>
+    </main>
+  );
+}
