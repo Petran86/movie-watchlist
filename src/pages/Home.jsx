59260a6e77a33381bd2d79b68838dce92d55b724
Petros Kouzoupis <peterkuzu@gmail.com>
2025-03-19 15:01:47 +0200

    Added Layout for shared header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,11 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <section>
-        <form action="">
-          <input type="text" />
-          <button>Search</button>
-        </form>
-      </section>
-    </main>
+    <section>
+      <form action="">
+        <input type="text" />
+        <button>Search</button>
+      </form>
+    </section>
   );
 }
