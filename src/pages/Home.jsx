cc3378cf279a9fe271a7595501cb47b743e55510
Petros Kouzoupis <peterkuzu@gmail.com>
2025-03-25 23:15:16 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 // import nodata from "../images/no-data-initial.png";
 
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDg1MjJkOGM1YzRkMDk4N2MyNjYyZmNkMDY3MjIwNyIsIm5iZiI6MTc0Mjg5MTcxNi4zNzEsInN1YiI6IjY3ZTI2YWM0NDQwZjMxMWFjZTc1ZGVlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qq1Buw6cx8bgid3irjBB215w--FHPzkPz5XYFD-hsKk"; //TMDB key
+
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState("");
